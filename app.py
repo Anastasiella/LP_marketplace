a845b5b858a4f7675f24b6af374e669f938be3b7
Anastasia <an-astasi-ya@mail.ru>
2020-09-28 22:02:18 +0300

    Create template project

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+
+app = Flask(__name__)
+app.config.from_object('config.DevelopmentConfig')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import User
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
