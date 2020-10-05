3a40d8ba31ba8d355d3b5973e5135e71439b93ff
Anastasia <an-astasi-ya@mail.ru>
2020-10-05 22:53:44 +0300

    Update templates, add category and product view

--- app.py
+++ app.py
@@ -12,14 +12,14 @@ from models import User, Category, Product
 
 @app.route('/')
 def get_index():
-    #all_category = Category.query.all()
-    #for i in all_category:
-       # print(i)
-    all_category = Category.query.with_entities(Category.name, Category.image).all()
+    all_category = Category.query.with_entities(Category.name, Category.image, Category.category_id).all()
     return render_template('index.html', categories=all_category)
 
 
-@app.route('/items/', methods=['GET'], strict_slashes=False)
-def get_items():
-    return render_template('items.html')
+@app.route('/category/<int:category_id>', methods=['GET'], strict_slashes=False)
+def get_product_by_category(category_id):
+    products = Product.query.filter_by(category_id=category_id).all()
+    for h in products:
+        print(h.name)
+    return render_template('category.html', test=products)
 
