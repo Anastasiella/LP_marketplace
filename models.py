8c566348a18f676e55819670fb05cd3fb96a1f9e
Anastasia <an-astasi-ya@mail.ru>
2020-10-10 12:50:58 +0300

    Update models.py

--- models.py
+++ models.py
@@ -21,13 +21,16 @@ class Category(db.Model):
     category_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30))
     image = db.Column(db.String(70))
+    category_type = db.Column(db.String(15))
 
-    def __init__(self, category_id, name, image):
+    def __init__(self, category_id, name, image, category_type):
         self.name = name
         self.category_id = category_id
         self.image = image
+        self.category_type = category_type
 
     def __repr__(self):
+
         return f'<Category {self.category_id}>'
 
 
