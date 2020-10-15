45d35a329dd174a2b8ab0a5019c2f99febb9cef8
Anastasia <an-astasi-ya@mail.ru>
2020-10-15 22:58:50 +0300

    Udpate config.py and sync __init__.py for master and Artem branch. Update base html

--- marketplace/__init__.py
+++ marketplace/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_login import LoginManager, current_user, login_user, login_required, logout_user, UserMixin
+
 from marketplace.db import db
 
 from marketplace.category.views import category_blueprint
 from marketplace.general.views import general_blueprint
+from marketplace.user.views import user_blueprint
+
+from marketplace.user.models import User
 
 
 app = Flask(__name__)
@@ -10,8 +15,17 @@ app.config.from_object('config.DevelopmentConfig')
 db.init_app(app)
 
 app.register_blueprint(category_blueprint, url_prefix='/category')
+app.register_blueprint(user_blueprint, url_prefix='/user')
 app.register_blueprint(general_blueprint, url_prefix='/')
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'user.login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 
 if __name__ == "__main__":
