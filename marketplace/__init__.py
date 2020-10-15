261c35f452e52e5ffea45ad65710e456c58bfd25
Anastasia <an-astasi-ya@mail.ru>
2020-10-15 21:45:30 +0300

    Add general_blueprint into core app

--- marketplace/__init__.py
+++ marketplace/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from marketplace.db import db
 
 from marketplace.category.views import category_blueprint
+from marketplace.general.views import general_blueprint
 
 
 app = Flask(__name__)
@@ -9,6 +10,8 @@ app.config.from_object('config.DevelopmentConfig')
 db.init_app(app)
 
 app.register_blueprint(category_blueprint, url_prefix='/category')
+app.register_blueprint(general_blueprint, url_prefix='/')
+
 
 
 if __name__ == "__main__":
