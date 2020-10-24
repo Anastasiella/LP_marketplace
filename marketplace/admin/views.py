6f0b19bbe557cdb901d7bd436912493fe555979c
Artem Khismatulln <a.khismatulli@gmail.com>
2020-10-24 21:05:45 +0500

    Внес корректировки в base.html и убрал лишнюю страницу админа

--- marketplace/admin/views.py
+++ marketplace/admin/views.py
@@ -3,10 +3,6 @@ from webapp.user.decorators import admin_required
 
 blueprint = Blueprint('admin', __name__, url_prefix='/admin')
 
-@blueprint.route('/')
-@admin_required
-def admin_index():
-    title = "Редактирование категорий"
-    return render_template('admin/index.html', page_title= title)
+
     
 
