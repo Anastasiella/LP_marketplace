c76b10e045c4df3cdc0baa67e306f534141beaeb
Anastasia <an-astasi-ya@mail.ru>
2020-10-28 22:50:51 +0300

    Rename jinja filters

--- marketplace/utils/filters.py
+++ marketplace/utils/filters.py
@@ -1,6 +1,6 @@
 from .. import app
 
 
-@app.template_filter('format_data_to_hr')
-def format_data_to_hr(date_of_item):
+@app.template_filter('format_data_to_human_readable')
+def format_data_to_human_readable(date_of_item):
     return date_of_item.strftime("%Y-%m-%d %H:%M:%S")
